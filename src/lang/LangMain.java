38665f0c79445a16da28f473e1afc7340769508c
thrkrif <103479629+thrkrif@users.noreply.github.com>
2024-09-24 22:21:16 +0900

    24/09/24

--- src/lang/LangMain.java
+++ src/lang/LangMain.java
@@ -1,2 +1,9 @@
-package lang;public class LangMain {
+package lang;
+
+public class LangMain {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        System.out.println("hello java");
+        System.out.println("hello java");
+    }
 }
